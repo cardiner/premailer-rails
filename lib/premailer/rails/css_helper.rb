a943d91a99e3fe836e227bd4877d74346f787d4d
Toms <cardiner@gmail.com>
2017-01-17 13:53:44 +0200

    Update css_helper.rb

--- lib/premailer/rails/css_helper.rb
+++ lib/premailer/rails/css_helper.rb
@@ -26,6 +26,7 @@ class Premailer
       private
 
       def css_urls_in_doc(doc)
+        raise
         doc.search('link[@rel="stylesheet"]:not([@data-premailer="ignore"])').map do |link|
           if link.respond_to?(:remove)
             link.remove
