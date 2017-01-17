0b930ff1fa21870ef5f87f6b0021f51084bda7a2
Toms <cardiner@gmail.com>
2017-01-17 14:11:55 +0200

    Update customized_premailer.rb

--- lib/premailer/rails/customized_premailer.rb
+++ lib/premailer/rails/customized_premailer.rb
@@ -11,6 +11,7 @@ class Premailer
         @options = Rails.config.merge(with_html_string: true)
         Premailer.send(:include, Adapter.find(Adapter.use))
         doc = load_html(html)
+        binding.pry
         options = @options.merge(css_string: CSSHelper.css_for_doc(doc))
 
         super(doc.to_s, options)
